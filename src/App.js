546c276de57360c0becedbf07ed4124ce85c73f0
Ali Kagan Yilmaz <kaganylmz12@gmail.com>
2022-06-22 19:35:41 +0300

    Add operating works

--- src/App.js
+++ src/App.js
@@ -3,23 +3,32 @@ import Dashboard from './components/Dashboard';
 import Functions from './components/Functions';
 import React,{ useState } from 'react';
 function App() {
+  const functions = ['+','-','x', '/']
   const [calc, setCalc]= useState('')
-  const [result,setResult]=useState('')
   const updateCalc = x => {
+  
+    if((functions.includes(x) && calc ==='') || (functions.includes(x)&& functions.includes(calc.slice(-1)))) {return {} }
     setCalc(calc+x)
+    
+  } 
+  const calculateValues = () => {
+    setCalc(eval(calc))
+  }
+  const Deletion = x => {
+    setCalc(x)
   }
   
   return (
     <div className="flex flex-col items-center mt-32 ">
         <div className='flex bg-black w-96 h-16 justify-end text-white pr-2 pt-5 mb-5 rounded-xl shadow-2xl shadow-gray-500'>
-          {result ? '0' : calc}
+          {'' ? '0' : calc}
           </div>
         <div className='flex shadow-2xl shadow-gray-500'>
          
             <div className='bg-orange-400 '>
             <Functions click={()=> updateCalc('+')} func={'+'} />
             <Functions click={()=> updateCalc('-')} func={'-'} />
-            <Functions click={()=> updateCalc('x')} func={'x'} />
+            <Functions click={()=> updateCalc('*')} func={'x'} />
             <Functions click={()=> updateCalc('/')} func={'/'} />
             <Functions click={()=> updateCalc('.')} func={'.'} />
             </div>
@@ -29,7 +38,7 @@ function App() {
             <Dashboard click={()=> updateCalc('4')} digit={4}/>
             <Dashboard click={()=> updateCalc('7')} digit={7}/>
             <Dashboard click={()=> updateCalc('(')} digit={'('}/>
-            <Dashboard />
+            <Dashboard click={()=> Deletion(calc.slice(0,calc.length-1))} digit={'DEL'} />
           </div>
 
           <div>               
@@ -48,7 +57,7 @@ function App() {
             <Dashboard click={()=> updateCalc('9')} digit={9}/>
             <Dashboard click={()=> updateCalc(')')} digit={')'}/>
             <div className='bg-orange-200 '>
-            <Functions click={()=> {}} func={'='} />
+            <Functions click={()=> calculateValues() } func={'='} />
             </div>
           </div>
     
